75e33b280061ae1baf39e2df8086185146291bb7
Diego Ragosta <di154020@ucf.edu>
2026-06-23 13:38:02 -0400

    Add topic routes to the server

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Topic Routes
+app.use('/api/topics', require('./routes/topicRoutes'));
+
 app.get('/', (req, res) => {
   res.json({ message: 'API is running' });
 });
