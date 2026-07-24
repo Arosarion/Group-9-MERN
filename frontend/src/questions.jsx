af7107080d7d2c151b2222a21df7a0ea323d4cd5
Kev <kevblanco712@gmail.com>
2026-07-24 04:22:53 -0400

    Add files via upload

---
+++ frontend/src/questions.jsx
@@ -0,0 +1,121 @@
+<>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Practice Exam</title>
+  <link rel="stylesheet" href="style.css" />
+  <div className="stars" />
+  <div className="stars2" />
+  <div className="shooting-star" />
+  <div className="container">
+    <header className="exam-header">
+      <h1>Quiz Check</h1>
+      <div className="exam-info">
+        <span>Questions: 5</span>
+      </div>
+    </header>
+    <section className="question-card">
+      <h2>Question 1</h2>
+      <p className="question">What is this bone?</p>
+      <label className="option">
+        <input type="radio" name="q1" />
+        Option 1
+      </label>
+      <label className="option">
+        <input type="radio" name="q1" />
+        Option 2
+      </label>
+      <label className="option">
+        <input type="radio" name="q1" />
+        Option3
+      </label>
+      <label className="option">
+        <input type="radio" name="q1" />
+        Option 4
+      </label>
+    </section>
+    <section className="question-card">
+      <h2>Question 2</h2>
+      <p className="question">Where is this organ?</p>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 1
+      </label>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 2
+      </label>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 3
+      </label>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 4
+      </label>
+    </section>
+    <section className="question-card">
+      <h2>Question 3</h2>
+      <p className="question">How does this organ work?</p>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 1
+      </label>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 2
+      </label>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 3
+      </label>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 4
+      </label>
+    </section>
+    <section className="question-card">
+      <h2>Question 4</h2>
+      <p className="question">Where is that?</p>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 1
+      </label>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 2
+      </label>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 3
+      </label>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 4
+      </label>
+    </section>
+    <section className="question-card">
+      <h2>Question 5</h2>
+      <p className="question">Where is this thing?</p>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 1
+      </label>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 2
+      </label>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 3
+      </label>
+      <label className="option">
+        <input type="radio" name="q2" />
+        Option 4
+      </label>
+    </section>
+    <div className="buttons">
+      <button className="secondary-btn">Previous</button>
+      <button className="primary-btn">Next Question</button>
+    </div>
+  </div>
+</>
